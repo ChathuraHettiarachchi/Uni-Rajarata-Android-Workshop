499cdfefc0b1a86387ba4f325b63788f4c575be0
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2020-02-17 11:44:26 +0530

    start of main page

---
+++ 04.Landing page/Landing/app/src/main/java/com/choota/splash/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.choota.splash.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.choota.splash.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
