43016c0827a0cda95b611fa66b2492f75e5a6344
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2020-02-23 08:22:09 +0530

    added contact us

--- 06.API/API/app/src/main/java/com/choota/splash/helpers/Const.java
+++ 06.API/API/app/src/main/java/com/choota/splash/helpers/Const.java
@@ -1,7 +1,7 @@
 package com.choota.splash.helpers;
 
 public class Const {
-    public final static String API_KRY = "11245ed4526d2f51019f511d08a66728";
+    public final static String API_KRY = "1461f9e9eeb4b7b74055d3cc6ba54b81";
     public final static String BASE_URL = "https://api.themoviedb.org/3/";
     public final static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500";
 }
---
+++ 10.Toolbar.API/Toolbar/app/src/main/java/com/choota/splash/adapters/MoviesAdapter.java
@@ -0,0 +1,78 @@
+package com.choota.splash.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.choota.splash.R;
+import com.choota.splash.helpers.Const;
+import com.choota.splash.services.api.response.PopularMovie;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesHolder>{
+
+    private Context context;
+    private List<PopularMovie> items;
+
+    public MoviesAdapter(Context context, List<PopularMovie> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public MoviesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.widget_home_movie, parent, false);
+
+        return new MoviesAdapter.MoviesHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesHolder holder, int position) {
+        PopularMovie item = items.get(position);
+
+        holder.title.setText(item.getTitle());
+        holder.rating.setText(item.getVoteAverage() + "/10");
+        holder.genres.setText("Genres here");
+        holder.releaseDate.setText(item.getReleaseDate());
+        holder.description.setText(item.getOverview());
+
+        String imagePath = Const.IMAGE_BASE_URL+item.getPosterPath();
+        Picasso.get()
+                .load(imagePath)
+                .placeholder(R.drawable.movie_placeholder)
+                .error(R.color.error)
+                .into(holder.poster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items != null ? items.size():0;
+    }
+
+    public class MoviesHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, rating, releaseDate, genres, description;
+        public RoundedImageView poster;
+
+        public MoviesHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.txtTitle);
+            genres = itemView.findViewById(R.id.txtGenres);
+            rating = itemView.findViewById(R.id.txtRating);
+            releaseDate = itemView.findViewById(R.id.txtReleaseDate);
+            description = itemView.findViewById(R.id.txtDescription);
+            poster = itemView.findViewById(R.id.imgPoster);
+        }
+    }
+}
